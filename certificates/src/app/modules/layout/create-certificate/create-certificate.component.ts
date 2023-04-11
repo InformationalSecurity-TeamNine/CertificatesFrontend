d01c2a52e8d26930b24e75c422a769b76b95bdd7
Nebojša Vuga <72966399+nebojsavuga@users.noreply.github.com>
2023-04-11 17:36:19 +0200

    Generated component for creating certificates

---
+++ certificates/src/app/modules/layout/create-certificate/create-certificate.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-certificate',
+  templateUrl: './create-certificate.component.html',
+  styleUrls: ['./create-certificate.component.css']
+})
+export class CreateCertificateComponent {
+
+}
