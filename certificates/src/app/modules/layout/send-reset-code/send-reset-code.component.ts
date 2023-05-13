54c7b5167e005662319d291d4d2e0faee721df17
Bogdan <bogdanjanosevic@gmail.com>
2023-05-13 14:45:48 +0200

    Big bugs fixed

--- certificates/src/app/modules/layout/send-reset-code/send-reset-code.component.ts
+++ certificates/src/app/modules/layout/send-reset-code/send-reset-code.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { UserService } from '../../unregistered-user/services/user.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ResetType } from 'src/app/models/Users';
 
 @Component({
@@ -15,17 +15,19 @@ export class SendResetCodeComponent {
     verifyType: ""
   }
   CodeForm = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
+    email: new FormControl('', [Validators.required, Validators.email, Validators.minLength(5)]),
     verifyType: new FormControl('', [Validators.required]),
 
   });
-  constructor( private userService: UserService,private route: ActivatedRoute ) {}
+  constructor( private router: Router, private userService: UserService,private route: ActivatedRoute ) {}
 send(){
+  if (!this.CodeForm.valid){return;}
   console.log(this.CodeForm.value.verifyType);
   this.resetType.verifyType = this.CodeForm.value.verifyType;
   this.userService.sendResetCode( this.CodeForm.value.email, this.resetType)
           .subscribe(
             (res) => {
+              this.router.navigate(['/reset-password/' + this.CodeForm.value.email]);
               
             }
           );
